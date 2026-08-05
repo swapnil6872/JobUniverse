4e8d6a1a38998755c0fa0a239cef8e0bc489943a
Swapnil Gamit <gamitswapnil7@gmail.com>
2026-08-06 01:48:20 +0530

    Fix Vite favicon path and enhance responsive footer

--- client/src/layouts/Footer.jsx
+++ client/src/layouts/Footer.jsx
@@ -1,54 +1,92 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import{Instagram ,Facebook,Twitter,Linkedin } from 'lucide-react'
+import { Instagram, Facebook, Twitter, Linkedin } from 'lucide-react'
 import playStore from "../assets/img/playstore.svg";
 import appStore from "../assets/img/appstore.svg";
 
 function Footer() {
   return (
-    
-    <footer className='bg-[#333333] text-white font-[400] line-height[1.14] text-[14px]'>
-      <div className='flex flex-col justify-between items-center max-w-7xl mx-auto px-4 py-6'>
-        <div className='grid grid-cols-2 md:grid-cols-4 gap-6 w-full mb-6'>
-          <div className='flex flex-col gap-2'>
-              <Link>About us</Link>
-              <Link>We're hiring</Link>
-              <Link>Hire Interns for your company</Link>
-              <Link>Post a job</Link>
+    <footer className="bg-[#333333] text-white text-sm">
+      <div className="max-w-7xl mx-auto px-4 py-8">
+
+        {/* Links */}
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-8 pb-8 border-b border-gray-600">
+          <div className="flex flex-col gap-3">
+            <Link>About us</Link>
+            <Link>We're hiring</Link>
+            <Link>Hire Interns for your company</Link>
+            <Link>Post a job</Link>
           </div>
-          <div className='flex flex-col gap-2'>
+
+          <div className="flex flex-col gap-3">
             <Link>Team Diary</Link>
             <Link>Blog</Link>
             <Link>Our Services</Link>
-            <Link>Free job Alert</Link>
+            <Link>Free Job Alert</Link>
           </div>
-          <div className='flex flex-col gap-2'>
+
+          <div className="flex flex-col gap-3">
             <Link>Terms & Conditions</Link>
-            <Link>Privicy</Link>
+            <Link>Privacy</Link>
             <Link>Contact Us</Link>
             <Link>Resume Maker</Link>
           </div>
-          <div className='flex flex-col gap-2'>
+
+          <div className="flex flex-col gap-3">
             <Link>Sitemap</Link>
-            <Link>Collage TPO</Link>
+            <Link>College TPO</Link>
             <Link>List of Companies</Link>
-            <Link>Jobs For Woman</Link>
+            <Link>Jobs For Women</Link>
           </div>
         </div>
 
-         {/* second section */}
-        <div className='flex flex-col md:flex-row justify-between items-center gap-4 w-full border-t pt-4'>
-          <div className='flex items-center gap-4'>
-            <Link> <img src={playStore} alt="Play Store"  className='w-32 h-10 object-contain'/> </Link>
-            <Link> <img src={appStore} alt="App Store" className='w-32 h-10 object-contain'/> </Link>
-            <Link><Instagram/></Link>
-            <Link><Facebook/></Link>
-            <Link><Twitter/></Link>
-            <Link><Linkedin/></Link>
+        {/* Bottom */}
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-6 pt-6">
+
+          {/* Left */}
+          <div className="flex flex-col sm:flex-row items-center gap-5">
+
+            <div className="flex gap-3">
+              <Link>
+                <img
+                  src={playStore}
+                  alt="Play Store"
+                  className="w-32 h-auto"
+                />
+              </Link>
+
+              <Link>
+                <img
+                  src={appStore}
+                  alt="App Store"
+                  className="w-32 h-auto"
+                />
+              </Link>
+            </div>
+
+            <div className="flex items-center gap-4">
+              <Link className="hover:text-[#00A5EC] transition-colors">
+                <Instagram size={20} />
+              </Link>
+
+              <Link className="hover:text-[#00A5EC] transition-colors">
+                <Facebook size={20} />
+              </Link>
+
+              <Link className="hover:text-[#00A5EC] transition-colors">
+                <Twitter size={20} />
+              </Link>
+
+              <Link className="hover:text-[#00A5EC] transition-colors">
+                <Linkedin size={20} />
+              </Link>
+            </div>
           </div>
-          <div className='flex items-center'>
-            <p> &copy; Copyright © 2025 JobUniverse. All rights reserved.</p>
-          </div>
+
+          {/* Copyright */}
+          <p className="text-center text-xs sm:text-sm text-gray-300">
+            © 2025 JobUniverse. All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
@@ -56,6 +94,3 @@ function Footer() {
 }
 
 export default Footer
-
-
-
